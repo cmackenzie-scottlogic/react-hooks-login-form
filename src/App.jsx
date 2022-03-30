5dd20e0a22d1b05d853a54bf7e3a9c2946696272
cjmackenzie40 <cjmackenzie40@gmail.com>
2022-03-30 14:54:47 +0100

    Opened and save. Prettier appears to be formatting differently when run on save

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import "./App.css";
 import LoginForm from "./components/loginForm/LoginForm.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>React form with hooks</h1>
-      <div>
-        <LoginForm />
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>React form with hooks</h1>
+			<div>
+				<LoginForm />
+			</div>
+		</div>
+	);
 }
 
 export default App;
